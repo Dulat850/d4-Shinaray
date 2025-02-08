8d6c777dd613bac586c76b31c5a35d48a7779e23
torekhan1337 <torekhan1337@gmail.com>
2025-02-08 09:19:51 +0500

    Update IReviewController.java

--- src/controllers/interfaces/IReviewController.java
+++ src/controllers/interfaces/IReviewController.java
@@ -1,8 +1,6 @@
-/*package controllers.interfaces;
+package controllers.interfaces;
 
 public interface IReviewController {
     String addReview(int userId, String comment, int rating);
     String getReviewsByUser(int userId);
-    String getReviewsByUser(int userId, int offset, int limit);
-    String getReviewsByUser(int userId, int offset, int limit, String sort);
-}*/
+}
